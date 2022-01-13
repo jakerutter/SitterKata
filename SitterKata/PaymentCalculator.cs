98ec21f6e57e0b2a3b397bd0e04e6f6d01b6987f
Jacob Rutter <rutter.jacob@gmail.com>
2022-01-13 11:37:38 -0500

    Initial commit. Added XUnit test framework and reference to SitterKata project. First test ensures one hour worked before bed time returns 12 dollars.

---
+++ SitterKata/PaymentCalculator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SitterKata
+{
+    public class PaymentCalculator
+    {
+        const int preBedTimePay = 12;
+
+        public int CalculateEarnings()
+        {
+            return preBedTimePay;
+        }
+    }
+}
