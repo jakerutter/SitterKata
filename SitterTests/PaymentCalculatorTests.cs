3bd82ee49a0f80d880afdff946c04f234b547d46
Jacob Rutter <rutter.jacob@gmail.com>
2022-01-13 11:39:19 -0500

    Added Xunit test proj and reference

---
+++ SitterTests/PaymentCalculatorTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using SitterKata;
+using Xunit;
+
+namespace SitterTests
+{
+    public class PaymentCalculatorTests
+    {
+        [Fact]
+        public void BabysitterWorksOneHourBeforeBedtimeAndReceives12Dollars()
+        {
+            PaymentCalculator paymentCalc = new PaymentCalculator();
+
+            Assert.Equal(12, paymentCalc.CalculateEarnings());
+        }
+    }
+}
